78118c34b06978ace61b8f0a4e5cb623695ddcb7
paulopez78 <pau.lopez.78@gmail.com>
2020-04-26 23:26:16 +0200

    more fixes before final demo

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ tag=${1:-0.1}
 rm -rf ./build
 dotnet publish votingapp -o ./build
 docker build -f votingapp/Dockerfile -t paulopez/votingapp:$tag .
-docker build -f kurl/Dockerfile -t paulopez/kurl:$tag .
 docker push paulopez/votingapp:$tag
--- commands.sh
+++ commands.sh
@@ -3,27 +3,27 @@
 # ---------------------DEMO 1: outside/inside-----------------------------------
 #https://gist.github.com/BretFisher/5e1a0c7bcca4c735e716abf62afad389
 screen ~/Library/Containers/com.docker.docker/Data/vms/0/tty
+screen -r
 docker run -it --rm --privileged --pid=host justincormack/nsenter1
-# show ps aux from outside and inside a container
 
 docker run -d --name=rabbit rabbitmq
-docker exec -it rabbit bash
+docker exec -it rabbit sh -c "apt-get update && apt-get install iproute2 -y && bash"
 
-docker run -d --name webserver nginx
-docker exec -it webserver sh -c "apt-get update && apt-get install -y procps && bash"
-
+# show ps aux from outside and inside a container
+# hostname -i, hostname
+# show root file systems
 
 #----------------------------- DEMO 2: run apps --------------------------------
 ./cleanup.sh
-docker run -d -p 15672:15672 rabbitmq-management
+docker run -d -p 15672:15672 rabbitmq:management
 docker run -d -p 8080:80 nginx
 
 #--------------------------- DEMO 3: docker basic commands --------------------- 
 ./cleanup.sh
 docker version
-docker system info
 
 docker pull rabbitmq
+docker pull rabbitmq:3.7
 docker images 
 
 # container lifecycle
@@ -62,8 +62,10 @@ docker ps -s
 docker commit $container_id paulopez/kurl:0.1
 docker image inspect paulopez/kurl:0.1 | jq -r '.[0].RootFS'
 docker image inspect ubuntu:18.04 | jq -r '.[0].RootFS'
+docker run -it paulopez/kurl:0.1
 
 docker build -f kurl/Dockerfile -t paulopez/kurl:0.1 .
+docker run paulopez/kurl:0.1 curl abax.cloud
 docker push paulopez/kurl:0.1
 
 #--------------------- DEMO 6: dockerize your app ---------------------------------
